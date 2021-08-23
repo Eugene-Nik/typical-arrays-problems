bb27d02cbd8420dc5ea9fdc0bd0a10ba415fe3e5
Eugene-Nik <70854623+Eugene-Nik@users.noreply.github.com>
2021-08-23 15:15:48 +0300

    feat: all 63 tests passed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+function sort(a, b) {
+    if (a < b) {
+        return -1;
+    } else if (b < a) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 exports.min = function min (array) {
-  return 0;
+    if ( array == null || array.length == 0 ){
+        return 0;
+    } else {
+       return array.sort(sort).shift();
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( array == null || array.length == 0 ){
+        return 0;
+    } else {
+       return array.sort(sort).pop();
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == null || array.length == 0 ){
+        return 0;
+    } else {
+        let summ = 0;
+        for (let i = 0; i <= array.length - 1; i++){
+        summ = summ + array[i];
+        } return  summ / array.length;
+    } 
 }
